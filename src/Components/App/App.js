7df3c729a03a8b86142950c8bc4faaa81f7a5722
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-04-01 08:36:45 +0100

    Pass playlistName and playlistTrack states from App to Playlist component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,7 +25,7 @@ constructor(props) {
         <SearchBar />
       <div className="App-playlist">
         <SearchResults searchResults={this.state.searchResults}/>
-        <Playlist />
+        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
       </div>
       </div>
     </div>
